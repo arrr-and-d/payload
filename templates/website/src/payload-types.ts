efa4d89badb1325ca1538ad453556ae69f93b39b
arrr-and-d <181251629+arrr-and-d@users.noreply.github.com>
2025-01-26 21:47:45 +0000

    registered new types

--- templates/website/src/payload-types.ts
+++ templates/website/src/payload-types.ts
@@ -52,7 +52,7 @@ export interface Config {
     header: HeaderSelect<false> | HeaderSelect<true>;
     footer: FooterSelect<false> | FooterSelect<true>;
   };
-  locale: null;
+  locale: 'en' | 'fr' | 'es' | 'it' | 'de' | 'ru';
   user: User & {
     collection: 'users';
   };
