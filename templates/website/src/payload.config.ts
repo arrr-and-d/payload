8367c6802526f9c55b634e1c15ed570c6b175943
arrr-and-d <181251629+arrr-and-d@users.noreply.github.com>
2025-01-26 21:48:30 +0000

    localization using configs from next-intl config

--- templates/website/src/payload.config.ts
+++ templates/website/src/payload.config.ts
@@ -16,6 +16,7 @@ import { Header } from './Header/config'
 import { plugins } from './plugins'
 import { defaultLexical } from '@/fields/defaultLexical'
 import { getServerSideURL } from './utilities/getURL'
+import { i18nOptions, localization } from './i18n'
 
 const filename = fileURLToPath(import.meta.url)
 const dirname = path.dirname(filename)
@@ -91,4 +92,7 @@ export default buildConfig({
     },
     tasks: [],
   },
+  localization: localization,
+
+  i18n: i18nOptions,
 })
