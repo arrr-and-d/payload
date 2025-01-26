a743c9dbd5ef3110ea50b2041a21091e056c9442
arrr-and-d <181251629+arrr-and-d@users.noreply.github.com>
2025-01-26 21:46:17 +0000

    HACK: Language switching hook WIP -> will be replaced by provider

---
+++ templates/website/src/hooks/useLanguageSwitch.ts
@@ -0,0 +1,46 @@
+// HACK: This is messy. Remove this entire file
+import { useLocale } from 'next-intl'
+import { notFound, usePathname, useRouter } from 'next/navigation'
+import { useState } from 'react'
+
+import { GET_PAGE_LOCALE_URL_ROUTE } from '@/constants/api-routes'
+
+const getTranslatedUrl = async (params: { newLocale: string; locale: string; slug: string }) => {
+  const searchParams = new URLSearchParams({
+    newLocale: params.newLocale,
+    locale: params.locale,
+    slug: params.slug,
+  })
+
+  const response = await fetch(`${GET_PAGE_LOCALE_URL_ROUTE}?${searchParams}`)
+  const data = await response.json()
+  return data.url
+}
+
+export const useLanguageSwitch = () => {
+  const [isLoading, setIsLoading] = useState(false)
+  const pathname = usePathname()
+  const locale = useLocale()
+  const router = useRouter()
+
+  const changeLanguage = async (newLocale: string) => {
+    setIsLoading(true)
+    const slug = pathname.split('/').pop() ?? ''
+
+    try {
+      const url = await getTranslatedUrl({ locale, newLocale, slug })
+      router.replace(url ? `/${newLocale}/${url}` : `/${newLocale}`)
+    } catch (error) {
+      console.error('Error checking translation:', error)
+      notFound()
+    } finally {
+      setIsLoading(false)
+    }
+  }
+
+  return {
+    changeLanguage,
+    isLoading,
+    locale,
+  }
+}
